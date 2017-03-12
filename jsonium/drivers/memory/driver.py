2882dbb0fb02bad5325b80072277d76eba0c0816
Ertugrul Keremoglu <ertugkeremoglu@gmail.com>
2017-03-12 23:56:55 +0300

    Implement methods for creation database.

--- jsonium/drivers/memory/driver.py
+++ jsonium/drivers/memory/driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os.path
 from jsonium.abstract import Driver
 from jsonium.storages import StorageFactory
 
@@ -11,3 +12,6 @@ class MemoryDriver(Driver):
         super(MemoryDriver, self).__init__(*args, **kwargs)
         storage_factory = StorageFactory()
         self.storage = storage_factory.build('jsonium.storages.MemoryStorage')
+
+    def create_database(self, db_object):
+        return self.storage.mkdir(os.path.join('databases', db_object.name))
--- jsonium/storages/memory/storage.py
+++ jsonium/storages/memory/storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os.path
 from jsonium.abstract import Storage
 
 
@@ -11,3 +12,13 @@ class MemoryStorage(Storage):
     def __init__(self, *args, **kwargs):
         super(MemoryStorage, self).__init__(*args, **kwargs)
         self.__root__ = {}
+
+    def mkdir(self, dir_fd, **kwargs):
+        sep = os.path.sep
+        path = dir_fd.split(sep)
+        depth = self.__root__
+        for segment in path:
+            if segment not in depth:
+                depth.__setitem__(segment, {})
+            depth = depth.get(segment, {})
+        return True
